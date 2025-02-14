0aa18b70bbfe0acea2b3300a802eee043fbd4e99
Precious422 <israelprecious62@gmail.com>
2025-02-14 01:04:16 +0100

    commiting

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -1,8 +1,1 @@
-from fastapi import APIRouter
-from .books import router as books_router
-from .stage2 import router as stage2_router  # Import the new router
-
-router = APIRouter()
-router.include_router(books_router)
-router.include_router(stage2_router)  # Register the new route
 
